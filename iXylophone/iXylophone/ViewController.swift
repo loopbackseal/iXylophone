72fb3e2575ddd245123be322d9f4244caf10a868
loopbackseal <young961027@gmail.com>
2021-12-22 15:53:52 +0900

    iXylophone Finished play sounds with .currentTitle!

---
+++ iXylophone/iXylophone/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  iXylophone
+//
+//  Created by Young Soo Hwang on 2021/12/21.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(sender.currentTitle!)
+    }
+    
+    func playSound(_ name: String) {
+        guard let url = Bundle.main.url(forResource: "Sounds/\(name)", withExtension: "wav") else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let player = player else { return }
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
+
